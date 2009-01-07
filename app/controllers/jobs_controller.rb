688c5aa6b982fdbffcf681af6777a0943ee4089a
surat pyari <surat@satish.(none)>
2009-01-07 11:31:46 +0530

    a bug removed

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,7 +5,7 @@ class JobsController < ApplicationController
   before_filter :job_user?, :only => [:destroy, :edit, :update]
   
   def index
-   @jobs = params[:type].match(/required/) ? RequiredJob.paginate(:page => params[:page] ? params[:page] : 1) : AvailableJob.paginate(:page => params[:page] ? params[:page] : 1)
+   @jobs = params[:type].match(/required/) ? RequiredJob.paginate(:page => params[:page] ? params[:page] : 1, :order => 'updated_at DESC') : AvailableJob.paginate(:page => params[:page] ? params[:page] : 1, :order => 'updated_at DESC')
   end
   
   def new

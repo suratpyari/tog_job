39d8b45ba9867666de01e2b6eb816d116453080b
surat pyari <surat@satish.(none)>
2009-01-07 11:35:25 +0530

    a bug removed

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -4,10 +4,10 @@ module JobsHelper
   def head
     head_string = ""
     if current_user
-      head_string = "#{params[:type].match(/required/) ? link_to('Jobs Required', new_job_path(:type => 'required'), :class => 'button add') : link_to('Post Jobs', new_job_path, :class => 'button add')}<br />"
+      head_string = "#{params[:type] and params[:type].match(/required/) ? link_to('Jobs Required', new_job_path(:type => 'required'), :class => 'button add') : link_to('Post Jobs', new_job_path, :class => 'button add')}<br />"
     end
-    head_string += "<h2> #{params[:type].match(/required/) ? 'Required Jobs' : 'Available Jobs'}</h2>"
-    head_string += params[:type].match(/required/) ? 'If you are a developer and wants a job then post requirements here' : 'If you are a company and wants to hire developers then Post Jobs here'
+    head_string += "<h2> #{params[:type] and params[:type].match(/required/) ? 'Required Jobs' : 'Available Jobs'}</h2>"
+    head_string += params[:type] and params[:type].match(/required/) ? 'If you are a developer and wants a job then post requirements here' : 'If you are a company and wants to hire developers then Post Jobs here'
     return head_string
   end
   

626c608bed68c76701072d5ff52da9f48d62e40e
surat pyari <surat@satish.(none)>
2009-01-07 11:37:37 +0530

    a bug removed

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -7,7 +7,7 @@ module JobsHelper
       head_string = "#{params[:type] and params[:type].match(/required/) ? link_to('Jobs Required', new_job_path(:type => 'required'), :class => 'button add') : link_to('Post Jobs', new_job_path, :class => 'button add')}<br />"
     end
     head_string += "<h2> #{params[:type] and params[:type].match(/required/) ? 'Required Jobs' : 'Available Jobs'}</h2>"
-    head_string += params[:type] and params[:type].match(/required/) ? 'If you are a developer and wants a job then post requirements here' : 'If you are a company and wants to hire developers then Post Jobs here'
+    head_string += "#{params[:type] and params[:type].match(/required/) ? 'If you are a developer and wants a job then post requirements here' : 'If you are a company and wants to hire developers then Post Jobs here'}"
     return head_string
   end
   
